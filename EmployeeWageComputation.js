88f8f4866792c280baebb8ba89f8fdc30a41c11a
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-23 19:11:31 +0530

    [Saif Ali] Add : Get Work Hours Added

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -9,20 +9,22 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 let empHrs = 0;
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_ABSENT:
-        empHrs = 0;
-        break;
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS; 
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS; 
-        break;
-    default:
-        break;
+function getWorkingHours(empCheck)
+{
+    switch (empCheck) {
+        case IS_ABSENT:
+            return 0;
+            break;
+       case IS_PART_TIME:
+           return PART_TIME_HOURS; 
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS; 
+            break;
+    }
 }
 
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs += getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR; 
-console.log("Emp Daily Wage: " + empWage);
+console.log("Emp Hrs: "+ empHrs +", Emp Wage: " + empWage);
