c5748e842d9ad38cede6d0c37033b68386d0cdb2
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-23 19:35:43 +0530

    [Saif Ali] Add : Calculate Wage till max hours abd max working days reached Added

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,14 +1,16 @@
 console.log("Welcome to Employee Wage Computation Program using Java Script")
 
-console.log("------UC4------");
+console.log("------UC5------");
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS= 20;
-let empHrs = 0;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+let totalEmpHrs = 0;
+let totalWorkinDays = 0;
 
 function getWorkingHours(empCheck)
 {
@@ -24,10 +26,11 @@ function getWorkingHours(empCheck)
             break;
     }
 }
-for(let day=0; day < NUM_OF_WORKING_DAYS; day++)
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkinDays < NUM_OF_WORKING_DAYS)
 {
+    totalWorkinDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR; 
-console.log("Emp Hrs: "+ empHrs +", Emp Wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR; 
+console.log("UC5 - Total Days : "+ totalWorkinDays +" Total Hrs : " + totalEmpHrs +" Total Emp Wage : " + empWage);
