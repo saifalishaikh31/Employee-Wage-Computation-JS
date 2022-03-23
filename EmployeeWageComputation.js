a9165749411073f77b324cf817d523f6fdaa6703
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-23 19:00:09 +0530

    [Saif Ali] Add : Calculate Employee daily wage Added

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,12 +1,28 @@
 console.log("Welcome to Employee Wage Computation Program using Java Script")
 
-const IS_ABSENT=0;
-let empcheck = Math.floor(Math.random()*10)%2;
-if(empcheck==IS_ABSENT)
-{
-    console.log("Employee is absent");
+console.log("------UC2------");
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHrs = 0;
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_ABSENT:
+        empHrs = 0;
+        break;
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS; 
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS; 
+        break;
+    default:
+        break;
 }
-else
-{
-console.log("Employee is present");
-}
+
+let empWage = empHrs * WAGE_PER_HOUR; 
+console.log("Emp Daily Wage: " + empWage);
