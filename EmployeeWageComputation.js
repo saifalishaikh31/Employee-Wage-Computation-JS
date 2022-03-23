b91e509e5c88bb1428185dd7eb7e4092e7eea5b0
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-23 19:22:03 +0530

    [Saif Ali] Add : Calculate Wage For Month Added

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,12 +1,13 @@
 console.log("Welcome to Employee Wage Computation Program using Java Script")
 
-console.log("------UC3------");
+console.log("------UC4------");
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS= 20;
 let empHrs = 0;
 
 function getWorkingHours(empCheck)
@@ -23,8 +24,10 @@ function getWorkingHours(empCheck)
             break;
     }
 }
-
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs += getWorkingHours(empCheck);
+for(let day=0; day < NUM_OF_WORKING_DAYS; day++)
+{
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR; 
 console.log("Emp Hrs: "+ empHrs +", Emp Wage: " + empWage);
